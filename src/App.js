c69cf558fc043c1ca5b915a4e1144a34cb478ed8
Mukul Howale <howalemukul@gmail.com>
2022-09-30 03:16:51 +0530

    Passing props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import Jokes from './components/Jokes'
-import '../styles/App.css';
+import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Jokes/>
-      <Jokes/>
-      <Jokes/>
-      <Jokes/>
-      <Jokes/>
+    <div className="app">
+      <Jokes
+        punch = "It's hard to explain puns to kleptomaniacs because they always take things literally."
+      />
+      <Jokes
+        setup = "How did the hacker escape the police?"
+        punch = "He just ransomware!"
+      />
+      <Jokes
+        setup = "Why don't pirates travel on mountain roads?"
+        punch = "Scurvy."
+      />
+      <Jokes
+        setup = "I got my daughter a fridge for her birthday."
+        punch = "I can't wait to see her face light up when she opens it."
+      />
+      <Jokes
+        setup = "What's the best thing about Switzerland?"
+        punch = "I don't know, but the flag is a big plus!"
+      />
     </div>
   );
 }
--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -1,9 +1,25 @@
 import React from "react";
+import '../styles/Jokes.css';
 
-export default function Jokes(){
+export default function Jokes(props){
+    let setup = 
+    <>
+        <p className="title">Setup : </p>
+        <p className="details">{props.setup}</p>
+    </>
+
+    let puncline = 
+    <>
+        <p className="title">Punchline : </p>
+        <p className="details">{props.punch}</p>
+    </>
+
+    if(props.setup == null || props.setup === "") setup = "";
+
     return(
-        <div>
-            
+        <div className="jokes">
+            <>{setup}</>
+            <>{puncline}</>
         </div>
     )
 }
